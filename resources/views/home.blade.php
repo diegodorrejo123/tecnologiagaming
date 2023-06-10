39ad0b7ca9dff457562f2ef834bcf2e8e33d827e
Diego Dorrejo <ddorrejo@isf.do>
2023-06-09 20:11:05 -0400

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,221 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        {{-- <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div> --}}
+
+        <h1 class="mb-3">
+            Los mejores teclados gaming para comprar: ocho recomendaciones para diferentes usuarios y presupuestos
+        </h1>
+        
+                <h3>
+                    1. Mars Gaming MKXTKL
+                </h3>
+                <img class="mb-2" width="500px" src="{{asset('images/Mars_Gaming_MKXTKL.jpg')}}" alt="">
+                <p>
+                    Una opción asequible y recomendada para aquellos que buscan un teclado mecánico sencillo para gaming es
+                    el compacto Mars Gaming MKXTKL. Con un precio de 27,99 euros, este teclado cuenta con interruptores
+                    azules OUTEMU SQ, que ofrecen una respuesta táctil y precisa. Además, presenta un formato TKL,
+                    retroiluminación con cinco colores y diez perfiles diferentes, reposamuñecas extraíble, función
+                    antighosting y un cable USB chapado en oro. Es una excelente opción para los jugadores que desean una
+                    experiencia de juego fluida sin comprometer su presupuesto.
+                </p>
+
+
+                <h3>
+                    2. Logitech G213
+                </h3>
+                <img class="mb-2" width="500px" src="{{asset('images/Logitech_G213.jpg')}}" alt="">
+
+                    <p>
+                        El teclado Logitech G213 (59 euros) es una opción versátil que se adapta tanto a la productividad como
+                        al gaming esporádico. Este teclado de membrana presenta un diseño sencillo pero duradero, ideal para un
+                        uso
+                        diario constante. Cuenta con características como anti-ghosting para evitar pulsaciones simultáneas no
+                        deseadas, un cable USB resistente y zonas de iluminación RGB vibrantes que añaden un toque visual
+                        atractivo.
+                    </p>
+                    <p>
+                        El Logitech G213 también incluye un reposamuñecas para brindar comodidad durante largas sesiones de
+                        juego o trabajo. Además, cuenta con controles multimedia integrados para facilitar el acceso rápido a
+                        funciones de reproducción de audio. A pesar de su aparente simplicidad, este teclado puede
+                        personalizarse según las preferencias individuales a través del software específico de Logitech para
+                        gaming. En resumen, el Logitech G213 es una opción confiable y funcional que combina características
+                        esenciales con un precio asequible.
+                    </p>
+
+
+
+                <h3>
+                    3. Razer Cynosa V2 Chroma RGB
+                </h3>
+                <img class="mb-2" width="500px" src="{{asset('images/Razer_Cynosa_V2.jpg')}}" alt="">
+                <p>
+                    Si estás buscando un teclado versátil para disfrutar de sesiones de juego ocasionales, pero que aún así
+                    brinde una experiencia satisfactoria, te recomendamos considerar el Razer Cynosa V2 Chroma RGB, con un
+                    precio de 49,99 euros. Este teclado utiliza tecnología de membrana para ofrecer una respuesta suave,
+                    silenciosa y rápida.
+                </p>
+                <p>
+                    A pesar de ser un teclado de membrana, el Razer Cynosa V2 Chroma incluye características propias del
+                    gaming, como la impresionante iluminación RGB y las 104 teclas programables. Además, cuenta con la
+                    capacidad de registrar hasta 10 pulsaciones de teclas simultáneamente y se conecta a través de un cable
+                    USB.
+                </p>
+                <p>
+                    Con el Razer Cynosa V2 Chroma, disfrutarás de una experiencia de juego satisfactoria sin tener que
+                    gastar una fortuna. Este teclado ofrece un equilibrio adecuado entre rendimiento, funcionalidad y
+                    precio, lo que lo convierte en una opción a tener en cuenta para los jugadores ocasionales que buscan un
+                    teclado versátil y asequible.
+                </p>
+                
+                
+                
+                
+                <h3>
+                    4. Logitech G513
+                </h3>
+                <img class="mb-2" width="500px" src="{{asset('images/Logitech_G513.jpg')}}" alt="">
+                <p>
+                    El teclado Logitech (146 euros) es una elección destacada que cuenta con switches mecánicos de alta
+                    calidad de marca, los Romer-G Táctil. Este teclado ofrece una experiencia de gaming completa y se
+                    distingue por su comodidad, gracias a su reposamanos extraíble de espuma viscoelástica y su construcción
+                    de aleación de aluminio, que brinda una mayor resistencia y durabilidad.
+                </p>
+                <p>
+                    El teclado también cuenta con una impresionante iluminación RGB LIGHTSYNC, que permite una
+                    personalización completa de los efectos de iluminación. Además, su conexión USB garantiza una conexión
+                    estable y de baja latencia. También dispone de teclas específicas para controles multimedia y de
+                    iluminación, lo que facilita el acceso rápido a las funciones deseadas.
+                </p>
+                <p>
+                    Además, este teclado incluye software propio para gaming, que proporciona opciones de personalización
+                    avanzadas y la posibilidad de crear perfiles específicos para diferentes juegos. En resumen, el teclado
+                    Logitech ofrece una combinación de rendimiento, comodidad y características destacadas que lo convierten
+                    en una opción sólida para los jugadores exigentes.
+                </p>
+                
+                
+                
+                <h3>
+                    5. HyperX Alloy Elite 2
+
+                </h3>
+                <img class="mb-2" width="500px" src="{{asset('images/HyperX_Alloy.jpg')}}" alt="">
+                <p>
+                    El teclado HyperX Alloy Elite 2 (127 euros) es una opción destacada para los entusiastas del gaming que
+                    buscan un rendimiento excepcional. Este teclado mecánico presenta switches de Cherry en tres variantes,
+                    siendo los Red y Brown especialmente adecuados para el uso en juegos. El HyperX Alloy Elite 2 ofrece una
+                    excelente relación calidad-precio.
+                </p>
+                <p>
+                    Destaca por su comodidad, especialmente diseñado para sesiones de juego prolongadas. Cuenta con un
+                    reposamuñecas ergonómico que brinda un apoyo adecuado. Además, ofrece funciones de control intuitivas,
+                    como botones multimedia especializados, una rueda y botones de acceso rápido. Estas características
+                    permiten ajustar el brillo, los efectos de iluminación y el modo de juego según las preferencias
+                    individuales. Además, el teclado cuenta con la función N-Key Rollover, lo que garantiza la detección
+                    precisa de cada pulsación incluso al presionar múltiples teclas simultáneamente.
+                </p>
+                <p>
+                    El HyperX Alloy Elite 2 presenta un diseño atractivo, combinando una sólida estructura de acero con
+                    teclas adicionales con textura en un elegante color titanio. Además, cuenta con efectos de iluminación
+                    RGB dinámicos y una barra de iluminación adicional que añade un toque visual impactante. También se
+                    incluye una herramienta de extracción de teclas HyperX para facilitar la limpieza y personalización del
+                    teclado.
+                </p>
+
+
+
+                <h3>
+                    6. Razer Huntsman v2
+                </h3>
+                <img class="mb-2" width="500px" src="{{asset('images/Razer_Huntsman_v2.jpg')}}" alt="">
+                <p>
+                    El teclado Razer Huntsman V2 (159 euros) es una opción premium para los usuarios que buscan un
+                    rendimiento excepcional en términos de precisión y velocidad, gracias a sus innovadores switches
+                    optomecánicos. Estos switches ofrecen una experiencia de pulsación óptima y una vida útil prolongada,
+                    brindando una ventaja competitiva en los juegos.
+                </p>
+                <p>
+                    Una de las características destacadas del Razer Huntsman V2 es la calidad de su diseño. Cuenta con una
+                    espectacular iluminación Razer Chroma con 38 zonas personalizables, lo que permite crear efectos de
+                    iluminación impresionantes y personalizados. Su estructura de aluminio garantiza una construcción sólida
+                    y duradera, mientras que el reposamuñecas de piel sintética proporciona comodidad durante largas
+                    sesiones de juego.
+                </p>
+                <p>
+                    Además, el teclado ofrece la capacidad de pulsar hasta 10 teclas simultáneamente, lo que se conoce como
+                    función N-Key Rollover, para un rendimiento sin interrupciones. También cuenta con un acceso fácil a la
+                    personalización, lo que permite adaptar el teclado a las preferencias individuales. Las teclas macro son
+                    otra característica clave que permite asignar comandos complejos y rápidos para una ventaja estratégica
+                    en los juegos.
+                </p>
+
+
+
+                <h3>
+                    7. ROG Strix Scope NX Deluxe
+                </h3>
+                <img class="mb-2" width="500px" src="{{asset('images/ROG_Strix_Scope.jpg')}}" alt="">
+                <p>
+                    El ROG Strix Scope NX Deluxe (131 euros) es un teclado mecánico que, a pesar de tener un layout
+                    completo, destaca por su relativo tamaño compacto. Es especialmente adecuado para juegos de disparos en
+                    primera persona (FPS) debido a la amplitud y ergonomía de su tecla Ctrl. Está disponible con varios
+                    tipos de switches Cherry, y en este caso, se menciona el switch rojo.
+                </p>
+                <p>
+                    Este teclado ofrece características adicionales que lo hacen sobresalir. Incluye una tecla dedicada para
+                    ocultar aplicaciones y silenciar el audio, brindando un acceso rápido y conveniente a estas funciones.
+                    La estructura del teclado está construida en aluminio, lo que garantiza una mayor durabilidad y
+                    estabilidad durante el uso.
+                </p>
+                <p>
+                    Además, el ROG Strix Scope NX Deluxe presenta iluminación Aura Sync RGB LED personalizable. Esto permite
+                    crear efectos de iluminación impresionantes y sincronizarlos con otros dispositivos compatibles con Aura
+                    Sync para lograr una experiencia visual inmersiva. El teclado también incluye software de
+                    personalización que permite ajustar y asignar funciones a las teclas según las preferencias
+                    individuales. Asimismo, se proporciona una herramienta para extraer las teclas, facilitando su limpieza
+                    y personalización.
+                </p>
+
+
+                <h3>
+                    8. Corsair K95 RGB Platinum
+                </h3>
+                <img class="mb-2" width="500px" src="{{asset('images/Corsair_K95.jpg')}}" alt="">
+                <p>
+                    El Corsair K95 RGB Platinum (199 euros) es un teclado mecánico de alta calidad que se encuentra
+                    disponible con los switches Cherry Brown o el extremadamente rápido Speed MX. Con un diseño elegante
+                    pero funcional, este teclado destaca por su construcción sólida, utilizando aluminio anodizado para
+                    brindar durabilidad y resistencia.
+                </p>
+                <p>
+                    El teclado se conecta al ordenador mediante un cable USB, lo que garantiza una conexión estable y de
+                    baja latencia. Además, cuenta con 6 teclas macro dedicadas, lo que ofrece un acceso rápido y conveniente
+                    a funciones y comandos personalizados. En total, el teclado cuenta con 110 teclas programables, lo que
+                    brinda una gran flexibilidad y versatilidad para adaptarlo a tus necesidades.
+                </p>
+                <p>
+                    La iluminación RGB del Corsair K95 RGB Platinum es otro aspecto destacado. Podrás ajustar la intensidad
+                    y los tonos de la iluminación para crear un ambiente visual personalizado y atractivo. Además, el
+                    teclado incluye un reposamuñecas que proporciona comodidad durante largas sesiones de juego o trabajo, y
+                    se puede extraer si se prefiere utilizar el teclado sin él.
+                </p>
+    </div>
+@endsection
